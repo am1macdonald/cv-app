1fc09743ac852b02c55fab7c921f1a910164229d
am1macdonald <am1macdonald@gmail.com>
2022-07-07 16:16:49 -0400

    first commit - build backbone for project - delete some boilerplate from 'create-react-app'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './app.css';
+import EducationForm from './components/EducationForm';
+import ExperienceForm from './components/ExperienceForm';
+import InfoForm from './components/InfoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      personalInfo : {
+        name: "",
+        email: "",
+        phoneNumber: ""
+      },
+      education: [] 
+      // format //
+      /* {
+        schoolName: "",
+        titleOfStudy: "",
+        dateOfStudy: ""
+      }*/,
+      experience: []
+      // format //
+      /* {
+        companyName: "",
+        positionTitle: "",
+        duties: "",
+        datesOfEmployment: ""
+      }*/,
+    }
+
+    this.updateInfo = this.updateInfo.bind(this);
+  }
+
+  updateInfo(name, email, phone) {
+    this.setState({
+      personalInfo : {
+        name: name,
+        email: email,
+        phoneNumber: phone
+      },
+      education: this.state.education,
+      experience: this.state.experience
+    });
+  }
+  newSchool() {
+
+  }
+
+  newExperience(companyName, positionTitle) {
+    return {
+      companyName: `${companyName}`,
+      positionTitle: `${positionTitle}`,
+      duties: "",
+      datesOfEmployment: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <h1>Resume Generator</h1>
+        <InfoForm onButtonClicked={this.updateInfo}/>
+        <EducationForm />
+        <ExperienceForm />
+      </div>
+    )
+  }
 }
 
 export default App;
